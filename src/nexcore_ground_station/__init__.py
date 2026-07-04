011e8cc4ccd9982277eb34025bcfea756137f5b9
Mahedi Islam Nadim <mahediislamnadim@gmail.com>
2026-07-04 20:21:24 +0600

    Reorganize into src/ package structure with pip-installable entry point

---
+++ src/nexcore_ground_station/__init__.py
@@ -0,0 +1,4 @@
+from .app import GroundStation, SerialConn, MAVLink, Theme, ScrollFrame
+from .app import main
+
+__all__ = ["GroundStation", "SerialConn", "MAVLink", "Theme", "ScrollFrame", "main"]
